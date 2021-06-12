b25275156228a0e81ae0e9b014a8f128cb378121
kimmymonassar <kimmymonassar@gmail.com>
2021-06-12 20:24:54 +0200

    uppdatera react, dart-sass, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["react-app"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true
+    }
+  },
+  "rules": {
+    "no-unused-vars": "off"
+  }
+}
